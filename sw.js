3a0038a88e786acc30b8d1a80e0ab45b6db5bc55
Venkat Nemana <venkat.nemana@gmail.com>
2023-04-04 19:46:54 -0700

    Up to Chapter 19

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v3';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -9,7 +10,8 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.jpeg',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    'pages/fallback.html'
 ];
 
 //install service worker
@@ -26,6 +28,15 @@ self.addEventListener('install', evt => {
 //activate event
 self.addEventListener('activate', evt => {
     //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+                );
+        })
+    );
 });
 
 //fetch event
@@ -33,7 +44,12 @@ self.addEventListener('fetch', evt => {
     //console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheResponse => {
-            return cacheResponse || fetch(evt.request);
-        })
+            return cacheResponse || fetch(evt.request).then(fetchResponse => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchResponse.clone());
+                    return fetchResponse;
+                })
+            });
+        }).catch(() => caches.match('/pages/fallback.html'))
     )
 });
