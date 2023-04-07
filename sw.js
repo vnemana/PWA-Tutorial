27adbc08f6b0d86e52050b1d690628ae4b19cf89
venkat <venkat.nemana@gmail.com>
2023-04-07 07:07:41 -0700

    fallback etc.

--- sw.js
+++ sw.js
@@ -14,6 +14,18 @@ const assets = [
     'pages/fallback.html'
 ];
 
+//cache size limit function
+const limitCacheSize = (name, size) => { 
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            keys.forEach(k => console.log(k.url));
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+};
+
 //install service worker
 self.addEventListener('install', evt => {
     //console.log('service worker has been installed'); 
@@ -27,7 +39,7 @@ self.addEventListener('install', evt => {
 
 //activate event
 self.addEventListener('activate', evt => {
-    //console.log('service worker has been activated');
+    // console.log('service worker has been activated');
     evt.waitUntil(
         caches.keys().then(keys => {
             //console.log(keys);
@@ -41,15 +53,19 @@ self.addEventListener('activate', evt => {
 
 //fetch event
 self.addEventListener('fetch', evt => {
-    //console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheResponse => {
             return cacheResponse || fetch(evt.request).then(fetchResponse => {
                 return caches.open(dynamicCacheName).then(cache => {
                     cache.put(evt.request.url, fetchResponse.clone());
+                    limitCacheSize(dynamicCacheName, 15);
                     return fetchResponse;
                 })
             });
-        }).catch(() => caches.match('/pages/fallback.html'))
+        }).catch(() => {
+            if(evt.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html');
+            }
+        })
     )
 });
